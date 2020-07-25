baf0dd2c932b6390156dc3149866668f6e455228
Martin Chorley <martin.chorley@gmail.com>
2020-07-25 21:31:00 +0100

    remove content and start from scratch

--- static/swnrec/js/secrets.js
+++
@@ -1,3 +0,0 @@
-var googleAnalytics = "UA-35250488-1";
-var api_key = "4d413f3aa9fcd65a9439e463654a2b49";
-var api_secret = "d9e81b6ff0e49fabe289fa51dae68216";
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: '_assets/js/main.js',
-    output: {
-        filename: 'tmp.js',
-        path: path.resolve(__dirname, 'tmp/')
-    }
-}
