2df200b31924bc54e387c7fdf1808ab478648c87
Martin Chorley <martin.chorley@gmail.com>
2021-10-07 22:50:51 +0100

    actually add the bit that does the thing

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,9 @@ module.exports = function(eleventyConfig) {
 
     const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
     const insertImage = require("./build/insertImage.js");
+    const markdownItFootnote = require("markdown-it-footnote");
+    const markdownItEmoji = require("markdown-it-emoji");
+    const markdownIt = require("markdown-it");
     
     eleventyConfig.addPassthroughCopy({"tmp/css": "css"});
     eleventyConfig.addPassthroughCopy({"src/img": "img"});
@@ -15,6 +18,18 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
     // readable dates
     eleventyConfig.addPlugin(pluginDate);
+
+      // add footnotes and emoji to the markdown parser
+    let markdownLib = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true,
+        typographer: true,
+    })
+    .use(markdownItFootnote)
+    .use(markdownItEmoji);
+
+    eleventyConfig.setLibrary('md', markdownLib);
 
     eleventyConfig.addShortcode("insertImage", insertImage.insertImage);
     eleventyConfig.addShortcode("insertBlogImage", insertImage.insertBlogImage);
